2ee987a10e2f0417131a947ffe545ceaa3934ac1
shahinmorve <133105811+shahinmorve@users.noreply.github.com>
2024-02-09 18:34:38 +0530

    restuarnt

--- src/component/basics/Hotel.js
+++ src/component/basics/Hotel.js
@@ -2,11 +2,32 @@ import React, { useState } from "react";
 import "./style.css";
 import Menu from "../Menuapi";
 import Menucard from "../Menucard";
+
+const uniqueList=new Set(Menu.map((curElem)=>{
+  return curElem.category;
+}))
+console.log(uniqueList)
 const Hotel = () => {
   const [menuData, setMenuData] = useState(Menu);
+  
+  const filterItem=(category)=>{
+    const updatedList=Menu.filter((curElem)=>{
+      return curElem.category===category;
+    })
+    setMenuData(updatedList)
+  };
 
   return (
     <>
+      <nav className="navbar">
+        <div className="btn-group">
+          <button className="btn-group__item" onClick={()=>filterItem ("breakfast")}>Breakfast</button>
+          <button className="btn-group__item" onClick={()=>filterItem ("lunch")}>lunch</button>
+          <button className="btn-group__item" onClick={()=>filterItem ("dinner")}>Dinner</button>
+          <button className="btn-group__item" onClick={()=>filterItem ("evening")}>evening</button>
+          <button className="btn-group__item" onClick={()=>setMenuData(Menu)}>All</button>
+        </div> 
+      </nav>
       <Menucard menuData={menuData} />
     </>
   );
